5afd1d2d8fd72e7d57e8e760bc6015983001a5ee
bouya-thiam <fatoubouya24021996@gmail.com>
2023-04-05 20:20:17 +0000

    html_css

--- tp_js/js/script5.js
+++ tp_js/js/script5.js
@@ -1,5 +1,5 @@
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const IMGPATH = "https://image.tmdb.org/t/p/w200";
 const SEARCHAPI =
 "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
@@ -9,22 +9,30 @@ fetch(APIURL)
 .then(data => {
     const dict_results = data["results"];
     const contener = document.getElementById("contener");
-    const films = document.getElementById("films");
+    const entete = document.getElementById("rechercher");
+    const films = document.getElementById("films"); //original_title, overview , vote_average
     console.log(dict_results);
-    dict_results.forEach ( element =>{
-        console.log(element["backdrop_path"]);
+    dict_results.forEach ( element =>{                              // img.src = IMGPATH+element.poster_path
+        
 
-        const div = document.createElement("div");
+        const film = document.createElement("div");
         const img = document.createElement("img");
-        const src = document.createElement('src');
-        const h3 = document.createElement("h3");
 
-    })
- 
+        //const src = document.createAttribute('src');
+        img.setAttribute("src",IMGPATH+element["poster_path"]);       // img.src = IMGPATH+element.poster_path
+        film.appendChild(img);
+        films.appendChild(film);
 
+       
+        const div_footer = document.createElement("div");
+         const h3 = document.createElement("h3");
 
+        contener.appendChild(entete);
+        contener.appendChild(films);
 
-})
+    });
+
+});
 .catch(error => console.error(error));
 
 
